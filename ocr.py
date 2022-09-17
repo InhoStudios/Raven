f6b363e5916572aa55f2a52adef2209fce0729a8
Andy Shen <andyshenproper@gmail.com>
2022-09-17 16:34:05 -0400

    Add post-processing steps

--- ocr.py
+++ ocr.py
@@ -1,14 +1,63 @@
 import cv2
 import pytesseract
 from pytesseract import Output
+import numpy as np
 
 def get_text_coord_pairs(opencvimg):
     d = pytesseract.image_to_data(opencvimg, output_type=Output.DICT)
     n_tokens = len(d['text'])
+    word_coords = []
     for i in range(n_tokens):
-        if int(d['conf'][i] > 60):
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            opencvimg = cv2.rectangle(opencvimg, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if (int(d['conf'][i] > 60)):
+            word = {'x': d['left'][i], 'y': d['top'][i], 'w': d['width'][i], 'h': d['height'][i], 'txt': d['text'][i]}
+            if (word['txt'] != ""):
+                word_coords.append(word)
+    return word_coords
 
-    cv2.imshow('img', opencvimg)
-    cv2.waitKey(0)
+def get_word_by_coords(x, y, img):
+    words = get_text_coord_pairs(img)
+    for word in words:
+        if (x >= word['x'] and x <= word['x'] + word['w'] and y >= word['y'] and y <= word['y'] + word['h']):
+            return word['txt']
+    return None
+
+# preprocessing functions from nanonets.com
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.diilate(image, kernel, iterations=1)
+
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations=1)
+
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
