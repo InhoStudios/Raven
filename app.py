215110d8b1a6c85458848f965c0aa2db25f5e66f
Andy Shen <andyshenproper@gmail.com>
2022-09-19 16:03:15 -0700

    Swap to webcam model for temporary testing

--- app.py
+++ app.py
@@ -7,25 +7,20 @@ def mouse_coords(event, x, y, flags, param):
         mouseX, mouseY = x, y
 
 if __name__ == "__main__":
-    img = cv2.imread('imgs/skew.jpeg')
-    deskewed = ocr.deskew(img)
-    cv2.imshow("title", deskewed)
-    cv2.waitKey(0)
-    # cap = cv2.VideoCapture(0)
-    # cv2.setMouseCallback('Input', mouse_coords)
+    cap = cv2.VideoCapture(0)
+    cv2.setMouseCallback('Input', mouse_coords)
 
-    # if (not cap.isOpened()):
-    #     raise IOError("Cannot open webcam")
+    if (not cap.isOpened()):
+        raise IOError("Cannot open webcam")
 
-    # while True:
-    #     ret, frame = cap.read()
-    #     frame = cv2.resize(frame, None, fx=0.5, fy=0.5,interpolation=cv2.INTER_AREA)
-    #     cv2.imshow('Input', frame)
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, None, fx=0.5, fy=0.5,interpolation=cv2.INTER_AREA)
+        cv2.imshow('Input', frame)
 
-    #     c = cv2.waitKey(1)
-    #     if c == 27:
-    #         break
-    #     print(ocr.get_word_by_coords(mouseX, mouseY, frame))
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
 
-    # cap.release()
-    # cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
